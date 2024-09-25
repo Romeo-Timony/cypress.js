14ec578cef499ea84046e9f4fdde46ce06d4e1ca
Roman T <roman.timoshenko@gmail.com>
2024-09-25 09:20:20 +0200

    Add files via upload

---
+++ login-QAstudio.cy.js
@@ -0,0 +1,56 @@
+describe('Проверка авторизации', function () {
+
+    it('Верный пароль и верный логин', function () {
+         cy.visit('https://login.qa.studio/'); 
+         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+
+         cy.get('#mail').type('german@dolnikov.ru'); 
+         cy.get('#pass').type('iLoveqastudio1'); 
+         cy.get('#loginButton').click(); 
+
+         cy.get('#messageHeader').contains('Авторизация прошла успешно');
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+     it('Верный логин и неверный логин', function () {
+        cy.visit('https://login.qa.studio/'); 
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+
+        cy.get('#mail').type('german@dolnikov.ru'); 
+        cy.get('#pass').type('iLoveqastudio7'); 
+        cy.get('#loginButton').click(); 
+
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Проверка что в логине есть собачка', function () {
+        cy.visit('https://login.qa.studio/'); 
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+
+        cy.get('#mail').type('germandolnikov.ru'); 
+        cy.get('#pass').type('iLoveqastudio7'); 
+        cy.get('#loginButton').click(); 
+
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Проверка восстановления пароля', function () {
+        cy.visit('https://login.qa.studio/'); 
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+
+        cy.get('#forgotEmailButton').click(); 
+
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+ })
+ 
